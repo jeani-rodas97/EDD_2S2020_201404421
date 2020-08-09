ef06b35fcd53c3582ad148d1c805de11d73cfc76
jeani-rodas97 <jenni1897@gmail.com>
2020-08-09 14:15:26 -0600

    Datos Estudiante

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    //system("color 57");
+    system("color 75");
+    //system("color 5e");
+    cout<<"\n\n\t ===========================================\n";
+    cout<<"\t|            DATOS DEL ESTUDIANTE           |\n";
+    cout<<"\t|===========================================|\n";
+    cout<<" \t|                                           |"<<endl;
+    cout<<" \t|  USAC                                     |"<<endl;
+    cout<<" \t|  Estructuras de datos                     |"<<endl;
+    cout<<" \t|  Seccion C                                |"<<endl;
+    cout<<" \t|  Jeanifer Alejandra Garcia Rodas          |"<<endl;
+    cout<<" \t|  201404421                                |"<<endl;
+    cout<<" \t|                                           |"<<endl;
+    cout<<"\t ===========================================\n";
+    return 0;
+}
