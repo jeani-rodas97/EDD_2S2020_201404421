290348121fca9f575dec51ec90765863084b58ed
jeani-rodas97 <jenni1897@gmail.com>
2020-08-09 19:39:43 -0600

    Menu editar

--- main.cpp
+++ main.cpp
@@ -4,8 +4,64 @@
 
 using namespace std;
 
+
+int menuEditarProy(void)
+{
+    int OpPrincipal;
+    system("color 75");
+    cout<<"\n\n\t ===========================================\n";
+    cout<<"\t|              ELIJA UNA OPCION             |\n";
+    cout<<"\t|===========================================|\n";
+    cout<<" \t|                                           |"<<endl;
+    cout<<" \t|  1. Agregar nivel                         |"<<endl;
+    cout<<" \t|  2. Editar nivel                          |"<<endl;
+    cout<<" \t|  3. Copiar nivel                          |"<<endl;
+    cout<<" \t|  4. Crear cant. pisos                     |"<<endl;
+    cout<<" \t|  5. Agregar objeto                        |"<<endl;
+    cout<<" \t|  6. Mover objeto                          |"<<endl;
+    cout<<" \t|  7. Eliminar objeto                       |"<<endl;
+    cout<<" \t|  8. Eliminar pared                        |"<<endl;
+    cout<<" \t|  9. Eliminar nivel                        |"<<endl;
+    cout<<" \t|  10. Eliminar proyecto                    |"<<endl;
+    cout<<" \t|  11. Salir                                |"<<endl;
+    cout<<" \t|                                           |"<<endl;
+    cout<<"\t ===========================================\n";
+
+    cin >> OpPrincipal;
+    switch (OpPrincipal)
+    {
+        case 1:
+            cout << "Opcion 1 " << endl;
+            break;
+
+        case 2:
+
+            break;
+
+        case 3:
+
+            break;
+
+        case 4:
+
+            break;
+
+        case 5:
+
+            break;
+
+        case 6:
+
+            break;
+
+        default: cout<< "Ingrese un numero correcto" << endl;
+
+    }
+}
+
 int menuPrincipal(void)
 {
+    int OpPrincipal;
     system("color 57");
     cout<<"\n\n\t ===========================================\n";
     cout<<"\t|              ELIJA UNA OPCION             |\n";
@@ -19,11 +75,42 @@ int menuPrincipal(void)
     cout<<" \t|  6. Cargar librerias                      |"<<endl;
     cout<<" \t|                                           |"<<endl;
     cout<<"\t ===========================================\n";
+
+    cin >> OpPrincipal;
+    switch (OpPrincipal)
+    {
+        case 1:
+            cout << "Opcion 1 " << endl;
+            break;
+
+        case 2:
+            system("cls");
+            menuEditarProy();
+            break;
+
+        case 3:
+
+            break;
+
+        case 4:
+
+            break;
+
+        case 5:
+
+            break;
+
+        case 6:
+
+            break;
+
+        default: cout<< "Ingrese un numero correcto" << endl;
+
+    }
 }
 
 int main()
 {
-    int OpPrincipal;
     int uno = 1;
     //system("color 57");
     system("color 75");
@@ -45,6 +132,6 @@ int main()
     system("cls");
 
     menuPrincipal();
-    cin >> OpPrincipal;
+
     return 0;
 }
