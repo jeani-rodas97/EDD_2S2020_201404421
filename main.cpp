0274d28b36fbd55b48f6a3734d97ff05ac1345e1
jeani-rodas97 <jenni1897@gmail.com>
2020-08-09 19:19:28 -0600

    Menu principal

--- main.cpp
+++ main.cpp
@@ -4,8 +4,27 @@
 
 using namespace std;
 
+int menuPrincipal(void)
+{
+    system("color 57");
+    cout<<"\n\n\t ===========================================\n";
+    cout<<"\t|              ELIJA UNA OPCION             |\n";
+    cout<<"\t|===========================================|\n";
+    cout<<" \t|                                           |"<<endl;
+    cout<<" \t|  1. Ver proyectos                         |"<<endl;
+    cout<<" \t|  2. Editar proyectos                      |"<<endl;
+    cout<<" \t|  3. Cargar proyecto                       |"<<endl;
+    cout<<" \t|  4. Graficar proyectos                    |"<<endl;
+    cout<<" \t|  5. Guardar proyectos                     |"<<endl;
+    cout<<" \t|  6. Cargar librerias                      |"<<endl;
+    cout<<" \t|                                           |"<<endl;
+    cout<<"\t ===========================================\n";
+}
+
 int main()
 {
+    int OpPrincipal;
+    int uno = 1;
     //system("color 57");
     system("color 75");
     //system("color 5e");
@@ -19,6 +38,13 @@ int main()
     cout<<" \t|  Jeanifer Alejandra Garcia Rodas          |"<<endl;
     cout<<" \t|  201404421                                |"<<endl;
     cout<<" \t|                                           |"<<endl;
+    cout<<" \t|  Presione 1 para continuar                |"<<endl;
     cout<<"\t ===========================================\n";
+
+    cin >> uno ;
+    system("cls");
+
+    menuPrincipal();
+    cin >> OpPrincipal;
     return 0;
 }
