71c124ad7b38c2856a37fd43805bf9a818b1f105
jeani-rodas97 <jenni1897@gmail.com>
2020-08-09 20:58:55 -0600

    Clase Menu

---
+++ Menu.h
@@ -0,0 +1,17 @@
+#ifndef MENU_H
+#define MENU_H
+
+
+class Menu
+{
+    public:
+        //Menu();
+        int Principal(void);
+        int EditarProy(void);
+
+    protected:
+
+    private:
+};
+
+#endif // MENU_H
--- main.cpp
+++ main.cpp
@@ -1,116 +1,14 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
+#include "Menu.h"
 
 using namespace std;
 
-
-int menuEditarProy(void)
-{
-    int OpPrincipal;
-    system("color 75");
-    cout<<"\n\n\t ===========================================\n";
-    cout<<"\t|              ELIJA UNA OPCION             |\n";
-    cout<<"\t|===========================================|\n";
-    cout<<" \t|                                           |"<<endl;
-    cout<<" \t|  1. Agregar nivel                         |"<<endl;
-    cout<<" \t|  2. Editar nivel                          |"<<endl;
-    cout<<" \t|  3. Copiar nivel                          |"<<endl;
-    cout<<" \t|  4. Crear cant. pisos                     |"<<endl;
-    cout<<" \t|  5. Agregar objeto                        |"<<endl;
-    cout<<" \t|  6. Mover objeto                          |"<<endl;
-    cout<<" \t|  7. Eliminar objeto                       |"<<endl;
-    cout<<" \t|  8. Eliminar pared                        |"<<endl;
-    cout<<" \t|  9. Eliminar nivel                        |"<<endl;
-    cout<<" \t|  10. Eliminar proyecto                    |"<<endl;
-    cout<<" \t|  11. Salir                                |"<<endl;
-    cout<<" \t|                                           |"<<endl;
-    cout<<"\t ===========================================\n";
-
-    cin >> OpPrincipal;
-    switch (OpPrincipal)
-    {
-        case 1:
-            cout << "Opcion 1 " << endl;
-            break;
-
-        case 2:
-
-            break;
-
-        case 3:
-
-            break;
-
-        case 4:
-
-            break;
-
-        case 5:
-
-            break;
-
-        case 6:
-
-            break;
-
-        default: cout<< "Ingrese un numero correcto" << endl;
-
-    }
-}
-
-int menuPrincipal(void)
-{
-    int OpPrincipal;
-    system("color 57");
-    cout<<"\n\n\t ===========================================\n";
-    cout<<"\t|              ELIJA UNA OPCION             |\n";
-    cout<<"\t|===========================================|\n";
-    cout<<" \t|                                           |"<<endl;
-    cout<<" \t|  1. Ver proyectos                         |"<<endl;
-    cout<<" \t|  2. Editar proyectos                      |"<<endl;
-    cout<<" \t|  3. Cargar proyecto                       |"<<endl;
-    cout<<" \t|  4. Graficar proyectos                    |"<<endl;
-    cout<<" \t|  5. Guardar proyectos                     |"<<endl;
-    cout<<" \t|  6. Cargar librerias                      |"<<endl;
-    cout<<" \t|                                           |"<<endl;
-    cout<<"\t ===========================================\n";
-
-    cin >> OpPrincipal;
-    switch (OpPrincipal)
-    {
-        case 1:
-            cout << "Opcion 1 " << endl;
-            break;
-
-        case 2:
-            system("cls");
-            menuEditarProy();
-            break;
-
-        case 3:
-
-            break;
-
-        case 4:
-
-            break;
-
-        case 5:
-
-            break;
-
-        case 6:
-
-            break;
-
-        default: cout<< "Ingrese un numero correcto" << endl;
-
-    }
-}
-
 int main()
 {
+    Menu menu;
+
     int uno = 1;
     //system("color 57");
     system("color 75");
@@ -131,7 +29,7 @@ int main()
     cin >> uno ;
     system("cls");
 
-    menuPrincipal();
+    menu.Principal();
 
     return 0;
 }
